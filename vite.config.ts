74dea37082282f18eb8e009be29fbb274c616c40
Mrluoying <694943553@qq.com>
2023-09-29 21:18:51 +0800

    build(deps): 添加路径别名跟添加环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+const resovePath = (src) => {
+  return path.resolve(src)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resovePath('./src')
+    }
+  }
 })
