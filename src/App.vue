3c1308f80ff682535be77630896f528df36cb908
Mrluoying <694943553@qq.com>
2023-09-29 18:08:27 +0800

    build(deps): 测试git校验

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    我是一个APP页面
-  </div>
+  <div>我是一个APP页面</div>
+  <span>测试git提交规范</span>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
