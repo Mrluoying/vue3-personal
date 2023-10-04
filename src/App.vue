dae1dbba4b0e70626da15c2f233897d7465c8d7c
Mrluoying <694943553@qq.com>
2023-10-04 21:47:21 +0800

    feat(utils): api统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-import request from './utils/request'
+// import request from './utils/request'
+import { reqLogin } from './api/user'
 import { onMounted } from 'vue'
 
 onMounted(() => {
-  request({
-    url: '/api/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
+  // request({
+  //   url: '/user/login',
+  //   method: 'post',
+  //   data: {
+  //     username: 'admin',
+  //     password: '111111',
+  //   },
+  // }).then((res) => {
+  //   console.log(res)
+  // })
+  reqLogin({
+    username: 'admin',
+    password: '111111',
   }).then((res) => {
     console.log(res)
   })
