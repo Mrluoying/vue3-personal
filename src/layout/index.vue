425e867a8377f5fce211fc4a31dc0f035d787c01
Mrluoying <694943553@qq.com>
2023-11-14 21:45:27 +0800

    feat(other): 添加layout布局

---
+++ src/layout/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout_container">
+    <div class="layout_slider">slider</div>
+    <div class="layout_tabbar">tabbar</div>
+    <div class="layout_main">main</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout_container {
+  width: 100%;
+  height: 100vh;
+  background-color: pink;
+  .layout_slider {
+    width: $base_menu_width;
+    height: 100vh;
+    background-color: $base_menu_background;
+  }
+  .layout_tabbar {
+    position: fixed;
+    width: calc(100% - $base_menu_width);
+    height: $base_tabbar_height;
+    top: 0;
+    left: $base_menu_width;
+    background-color: red;
+  }
+  .layout_main {
+    position: absolute;
+    width: calc(100% - $base_menu_width);
+    height: calc(100% - $base_tabbar_height);
+    top: $base_tabbar_height;
+    left: $base_menu_width;
+    padding: 20px;
+    // overflow: auto;
+  }
+}
+</style>
