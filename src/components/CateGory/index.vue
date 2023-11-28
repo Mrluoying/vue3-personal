728dfa974ac2f32509892cc8996e2d71dbc78bbf
Mrluoying <694943553@qq.com>
2023-11-28 10:39:01 +0800

    feat(other): 属性管理静态

---
+++ src/components/CateGory/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-card>
+    <el-form inline>
+      <el-form-item label="一级分类">
+        <el-select>
+          <el-option>1</el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="一级分类">
+        <el-select>
+          <el-option>1</el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="一级分类">
+        <el-select>
+          <el-option>1</el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Plus } from '@element-plus/icons-vue'
+</script>
 
+<!-- 需要添加一个div包裹一下，要不然首页中使用了过渡动画，会有如下警告 -->
+<!-- 
+  <Transition> renders non-element root node that cannot be animated. 这句话的意思是：
+  <Transition>中的组件渲染无法设置动画的非元素根节点
+-->
 <template>
-  <div>attr</div>
+  <div>
+    <CateGory></CateGory>
+    <el-card class="content_card">
+      <el-button type="primary" :icon="Plus">添加属性</el-button>
+      <el-table border class="table_container">
+        <el-table-column
+          label="序号"
+          type="index"
+          align="center"
+          width="80px"
+        ></el-table-column>
+        <el-table-column width="180px" label="属性名称"></el-table-column>
+        <el-table-column label="属性值名称"></el-table-column>
+        <el-table-column width="180px" label="操作"></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content_card {
+  margin-top: 10px;
+  .table_container {
+    margin: 10px 0;
+  }
+}
+</style>
